4e5058bb7f43da8c95614947b57adc9eda7c71e9
dmitrymakhotin <surfpandas@gmail.com>
2014-11-14 18:31:37 +0300

    new

--- homework_4/DiffLet.py
+++ homework_4/DiffLet.py
@@ -8,24 +8,34 @@ def func(s):
 # s = raw_input()
 # s = s.rsplit()
 
-s = 'aaa b a cc wwwww eee   abaaaaabbbbbab vnvv fgfgfgfg werwerwer axaxxxxxaxaxaxaxaxx'
-s = s.rsplit()
+s = 'aaa b a cc wwwww eee  yui  abaaaaabbbbbab vnvv fgfgfgfg werwerwer axaxxxxxaxaxaxaxaxx'
+# s = s.rsplit()
 # print s
 
-d = dict()
-for i in s:
-    num_of_sym = func(i)
-    if num_of_sym:
-        if num_of_sym not in d:
-            d[num_of_sym] = []
-            d[num_of_sym].append(i)
-        else:
-            d[num_of_sym].append(i)
+# def num(s):
+#     return len(set(s))
 
-
-e = ''
-for i in sorted(d.keys()):
-    for string in d[i]:
-        e = e+string+' '
+p = sorted(s.split(),key = lambda x: len(set(x)))
+e = " ".join(p)
 
 print e
+
+
+
+# d = dict()
+# for i in s:
+#     num_of_sym = func(i)
+#     if num_of_sym:
+#         if num_of_sym not in d:
+#             d[num_of_sym] = []
+#             d[num_of_sym].append(i)
+#         else:
+#             d[num_of_sym].append(i)
+#
+#
+# e = ''
+# for i in sorted(d.keys()):
+#     for string in d[i]:
+#         e = e+string+' '
+#
+# print e
