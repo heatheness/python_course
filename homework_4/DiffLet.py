a7ad9336722a89c7883b98e21ba702594125bcd8
dmitrymakhotin <surfpandas@gmail.com>
2014-11-15 09:43:54 +0300

    new

--- homework_4/DiffLet.py
+++ homework_4/DiffLet.py
@@ -6,7 +6,7 @@ def func(s):
     return len(set(s))
 
 # s = raw_input()
-# s = s.rsplit()
+# s = s.split()
 
 s = 'aaa b a cc wwwww eee  yui  abaaaaabbbbbab vnvv fgfgfgfg werwerwer axaxxxxxaxaxaxaxaxx'
 # s = s.rsplit()
@@ -20,22 +20,3 @@ e = " ".join(p)
 
 print e
 
-
-
-# d = dict()
-# for i in s:
-#     num_of_sym = func(i)
-#     if num_of_sym:
-#         if num_of_sym not in d:
-#             d[num_of_sym] = []
-#             d[num_of_sym].append(i)
-#         else:
-#             d[num_of_sym].append(i)
-#
-#
-# e = ''
-# for i in sorted(d.keys()):
-#     for string in d[i]:
-#         e = e+string+' '
-#
-# print e
